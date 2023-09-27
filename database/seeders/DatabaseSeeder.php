ddd6350a8a5213dc3b646e435a0363c629527bf6
NOOBHECMETAHKA <ebaltboupotha@gmail.com>
2023-09-27 11:58:22 +0300

    backup before migrate update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $cate = Category::factory(10)->make();
+        //dd($cate);
         // \App\Models\User::factory(10)->create();
     }
 }
